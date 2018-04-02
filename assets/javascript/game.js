839638f48980d2643b38e818dbd228af3dd0c42a
bill white <wmpwhite@msn.com>
2018-04-01 22:29:29 -0400

    commented files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,22 +3,26 @@
 
 $(document).ready(function() {
 
+    // declare global variables and set initial wins and losses
     var targetValue;
     var playerTotal;
     var crystalValue = [];
     var wins = 0;
     var losses = 0;
           
-
+    // overall function to control game
     function playGame() {
 
+        // initial values for variables
         crystalValue =[0,0,0,0];
         targetValue = 0;
         playerTotal = 0;
 
+        // write wins and losses onto page
         document.querySelector("#player-wins").innerHTML = wins;
-        document.querySelector("#player-losses").innerHTML = losses;        
+        document.querySelector("#player-losses").innerHTML = losses;
 
+        // function playAgain is called at the end of a game and resets variables and resets values displayed on the page
         function playAgain() {
             playerTotal = 0;
             document.querySelector("#player-total").innerHTML = playerTotal;
@@ -27,6 +31,7 @@ $(document).ready(function() {
             crystalValueGenerator();            
         }        
 
+        // function targetValueGenerator generates a random number between 19 and 120 as a target for the player
         function targetValueGenerator() {
             for (var i = 1; i <= 10; i++) {
                 targetValue = Math.floor(Math.random() * 120) + 1;                     
@@ -35,19 +40,22 @@ $(document).ready(function() {
                 }
             } 
         }
-
+        // calls the function to generate target value and displays that value on screen
         targetValueGenerator();        
         document.querySelector("#target-number").innerHTML = targetValue;
 
+        // function crystalValueGenerator generates random values between 1 and 12 for each "crystal" 
         function crystalValueGenerator() {
             for (var j =0; j <= 3; j++) {
                 crystalValue[j] = Math.floor(Math.random() * 12) + 1;
             }
         }
-
+        // calls the function to generate "crystal" values
         crystalValueGenerator();
         
-        
+        // This function checks to see if the user has won or lost the game with the most current click
+        // It also increments the wins or losses variables and updates the screen display of those variables, as appropriate
+        // finally, it alerts the win or loss and then calls the playAgain function (see above) 
         function gameLogic() {
             document.querySelector("#player-total").innerHTML = playerTotal;
             if (playerTotal === targetValue) {
@@ -66,9 +74,10 @@ $(document).ready(function() {
             }
         }
         
+        // makes initial display of player running total (as 0) 
         document.querySelector("#player-total").innerHTML = playerTotal;
 
-
+        // on click event updates player's running total and calls the gameLogic function
         $("body").on("click", "#c-1", function() {                       
             playerTotal = playerTotal + crystalValue[0];            
             gameLogic();
@@ -81,12 +90,10 @@ $(document).ready(function() {
         }).on("click", "#c-4", function() { 
             playerTotal = playerTotal + crystalValue[3];            
             gameLogic();
-        });
-
-        
+        });        
 
     }
-
+    // initializes first game
      playGame();
 
 });
