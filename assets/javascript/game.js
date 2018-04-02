d4483dec2bf26db1313950012dfbc96971c5c903
bill white <wmpwhite@msn.com>
2018-04-01 20:14:10 -0400

    push of functioning code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,42 @@
 // Javascript for Crystals Game
 
 
-// generate random number for guess
-
 $(document).ready(function() {
 
-  
+    var targetValue;
+    var playerTotal;
+    var crystalValue = [];
+    var wins = 0;
+    var losses = 0;
+          
 
     function playGame() {
 
-        var targetValue = 0;
-        var crystalValue =[0,0,0,0];
-        var playerTotal = 0;
-        var wins = 0;
-        var losses = 0;
+        crystalValue =[0,0,0,0];
+        targetValue = 0;
+        playerTotal = 0;
+
+        document.querySelector("#player-wins").innerHTML = wins;
+        document.querySelector("#player-losses").innerHTML = losses;        
+
+        function playAgain() {
+            playerTotal = 0;
+            document.querySelector("#player-total").innerHTML = playerTotal;
+            targetValueGenerator();
+            document.querySelector("#target-number").innerHTML = targetValue;
+            crystalValueGenerator();            
+        }        
 
         function targetValueGenerator() {
             for (var i = 1; i <= 10; i++) {
-                targetValue = Math.floor(Math.random() * 120) + 1;
-                console.log("targetValue = " + targetValue);     
+                targetValue = Math.floor(Math.random() * 120) + 1;                     
                 if (targetValue >= 19) {
                     i = 11;
                 }
             } 
         }
-        targetValueGenerator();
-        console.log("targetValue = " + targetValue);
 
+        targetValueGenerator();        
         document.querySelector("#target-number").innerHTML = targetValue;
 
         function crystalValueGenerator() {
@@ -36,44 +46,45 @@ $(document).ready(function() {
         }
 
         crystalValueGenerator();
-        console.log(crystalValue[0]);
-        console.log(crystalValue[1]);
-        console.log(crystalValue[2]);
-        console.log(crystalValue[3]);
-        console.log("--------------");
-        console.log(playerTotal);
-
-
-
-
-
-        $("body").on("click", "#c-1", function() {
-            playerTotal = playerTotal + crystalValue[0];
-            console.log(playerTotal);
-        }).on("click", "#c-2", function() {
-            playerTotal = playerTotal + crystalValue[1];
-            console.log(playerTotal);
-        }).on("click", "#c-3", function() {
-            playerTotal = playerTotal + crystalValue[2];
-            console.log(playerTotal);            
-        }).on("click", "#c-4", function() {
-            playerTotal = playerTotal + crystalValue[3];
-            console.log(playerTotal);
-        });
-
+        
+        
+        function gameLogic() {
+            document.querySelector("#player-total").innerHTML = playerTotal;
+            if (playerTotal === targetValue) {
+                wins++;                
+                document.querySelector("#player-wins").innerHTML = wins;
+                playerTotal = 0;
+                alert("Congratulations, you won!");
+                playAgain();
+            } 
+                else if (playerTotal > targetValue) {
+                losses++;                
+                document.querySelector("#player-losses").innerHTML = losses;
+                playerTotal = 0;
+                alert("Sorry, you lost.");
+                playAgain();
+            }
+        }
+        
         document.querySelector("#player-total").innerHTML = playerTotal;
 
-        if (playerTotal === targetValue) {
-            wins++;
-            document.querySelector("#player-wins").innerHTML = wins;
-            alert("Congratulations, you won!");
-        } 
-            else if (playerTotal > targetValue) {
-            losses++;
-            console.log(losses)
-            document.querySelector("#player-losses").innerHTML = losses;
-            alert("Sorry, you lost.");
-        }
+
+        $("body").on("click", "#c-1", function() {                       
+            playerTotal = playerTotal + crystalValue[0];            
+            gameLogic();
+        }).on("click", "#c-2", function() {            
+            playerTotal = playerTotal + crystalValue[1];            
+            gameLogic();
+        }).on("click", "#c-3", function() {             
+            playerTotal = playerTotal + crystalValue[2];            
+            gameLogic();
+        }).on("click", "#c-4", function() { 
+            playerTotal = playerTotal + crystalValue[3];            
+            gameLogic();
+        });
+
+        
+
     }
 
      playGame();
